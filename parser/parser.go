fccb85d683d6a75624739cc143d1565212c3cfc8
kiki-ki <malpie0619@gmail.com>
2021-10-23 21:00:54 +0900

    pass the test to add parse let statement

--- parser/parser.go
+++ parser/parser.go
@@ -9,7 +9,7 @@ import (
 type Parser struct {
 	l *lexer.Lexer
 
-	curToken token.Token
+	curToken  token.Token
 	peekToken token.Token
 }
 
@@ -28,5 +28,58 @@ func (p *Parser) nextToken() {
 }
 
 func (p *Parser) ParseProgram() *ast.Program {
-	return nil
+	program := &ast.Program{}
+	program.Statements = make([]ast.Statement, 0)
+
+	for !p.curTokenIs(token.EOF) {
+		s := p.parseStatement()
+		if s != nil {
+			program.Statements = append(program.Statements, s)
+		}
+		p.nextToken()
+	}
+
+	return program
+}
+
+func (p *Parser) parseStatement() ast.Statement {
+	switch p.curToken.Type {
+	case token.LET:
+		return p.parseLetStatement()
+	default:
+		return nil
+	}
+}
+
+func (p *Parser) parseLetStatement() *ast.LetStatement {
+	s := &ast.LetStatement{Token: p.curToken}
+	if !p.expectPeek(token.IDENT) {
+		return nil
+	}
+	s.Name = &ast.Identifier{Token: p.curToken, Value: p.curToken.Literal}
+	if !p.expectPeek(token.ASSIGN) {
+		return nil
+	}
+	// TODO: セミコロンまで式を呼び飛ばしてる
+	for !p.curTokenIs(token.SEMICOLON) {
+		p.nextToken()
+	}
+	return s
+}
+
+func (p *Parser) curTokenIs(t token.TokenType) bool {
+	return p.curToken.Type == t
+}
+
+func (p *Parser) peekTokenIs(t token.TokenType) bool {
+	return p.peekToken.Type == t
+}
+
+func (p *Parser) expectPeek(t token.TokenType) bool {
+	if p.peekTokenIs(t) {
+		p.nextToken() // ここで進める
+		return true
+	} else {
+		return false
+	}
 }
