8cdf74b2ad43ad8455b2cdf58229ba9e15f434e5
kiki-ki <malpie0619@gmail.com>
2021-12-08 23:09:28 +0900

    add bool and int obj

---
+++ object/object.go
@@ -0,0 +1,39 @@
+package object
+
+import "fmt"
+
+type ObjectType string
+
+const (
+	INTEGER_OBJ = "INTEGER"
+	BOOLEAN_OBJ = "BOOLEAN"
+)
+
+type Object interface {
+	Type() ObjectType
+	Inspect() string
+}
+
+type Integer struct {
+	Value int64
+}
+
+func (i *Integer) Inspect() string {
+	return fmt.Sprintf("%d", i.Value)
+}
+
+func (i *Integer) Type() string {
+	return INTEGER_OBJ
+}
+
+type Boolean struct {
+	Value bool
+}
+
+func (b *Boolean) Inspect() string {
+	return fmt.Sprintf("%t", b.Value)
+}
+
+func (i *Boolean) Type() string {
+	return BOOLEAN_OBJ
+}
